04b912e15d9537b44cb1a1a6ab7a2c8ed0bca1cf
bruno-netec <bruno@ne.team>
2024-07-01 11:33:19 -0300

    Validando form de contato para whatsApp

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 AOS.init();
+
+document.getElementById('whatsapp-form').addEventListener('submit', function(event){
+  event.preventDefault();
+
+  const name = document.getElementById('nome').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('telefone').value;
+  const message = document.getElementById('msg-contact').value;
+
+  const whatsappMessage = `Nome: ${name}%0AEmail: ${email}%0ATelefone: ${phone}%0AMensagem: ${message}`;
+  
+  const whatsappLink = `https://api.whatsapp.com/send/?phone=558196811663&text=${whatsappMessage}&type=phone_number&app_absent=0`
+
+  window.open(whatsappLink, '_blank');
+});
+
